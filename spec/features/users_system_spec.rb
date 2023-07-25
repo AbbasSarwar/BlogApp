8e9b342772facd6b9c422783c4245dda663b8071
AbbasSarwar <abbassarwar100@gmail.com>
2023-07-25 13:21:21 +0500

    Linters fixed

--- spec/features/users_system_spec.rb
+++ spec/features/users_system_spec.rb
@@ -1,19 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :system do
-    before(:each) do
-        @profile1 = User.create(name: "Tom", photo: "https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg",bio: "Teacher from Mexico.", post_counter: 0)
-        @profile2 = User.create(name: "Lilly", photo: "https://as2.ftcdn.net/v2/jpg/02/14/74/61/1000_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg",bio: "Teacher from Poland.", post_counter: 0)
-    end
+  before(:each) do
+    @profile1 = User.create(name: 'Tom',
+                            photo: 'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg',
+                            bio: 'Teacher from Mexico.',
+                            post_counter: 0)
+    @profile2 = User.create(name: 'Lilly',
+                            photo: 'https://as2.ftcdn.net/v2/jpg/02/14/74/61/1000_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg',
+                            bio: 'Teacher from Poland.',
+                            post_counter: 0)
+  end
   describe 'user index' do
     before(:each) do
-        visit users_path
+      visit users_path
     end
     it 'shows the user details' do
       expect(page).to have_content(@profile1.name)
       expect(page).to have_content(@profile2.name)
     end
-    
+
     it 'should show the profile picture for each user.' do
       expect(page).to have_xpath("//img[contains(@src,'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg')]")
     end
@@ -29,61 +35,64 @@ RSpec.describe 'User', type: :system do
   end
 
   describe 'user#show' do
-  before(:each) do
-    @jin = User.create(name: "Jin", photo: "https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg", bio: "Teacher from Mexico.", post_counter: 0)
-    @post1 = Post.create(title: 'title 1',
-                         text: 'post content 1',
-                         comments_counter: 0,
-                         likes_counter: 0,
-                         author_id: @jin.id)  
-    @post2 = Post.create(title: 'title 2',
-                         text: 'post content 2',
-                         comments_counter: 0,
-                         likes_counter: 0,
-                         author_id: @jin.id) 
-    @post3 = Post.create(title: 'title 3',
-                         text: 'post content 3',
-                         comments_counter: 0,
-                         likes_counter: 0,
-                         author_id: @jin.id) 
-    @post4 = Post.create(title: 'title 4',
-                         text: 'post content 4',
-                         comments_counter: 0,
-                         likes_counter: 0,
-                         author_id: @jin.id) 
-    visit user_path(@jin.id)
-  end
+    before(:each) do
+      @jin = User.create(name: 'Jin',
+                         photo: 'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg',
+                         bio: 'Teacher from Mexico.',
+                         post_counter: 0)
+      @post1 = Post.create(title: 'title 1',
+                           text: 'post content 1',
+                           comments_counter: 0,
+                           likes_counter: 0,
+                           author_id: @jin.id)
+      @post2 = Post.create(title: 'title 2',
+                           text: 'post content 2',
+                           comments_counter: 0,
+                           likes_counter: 0,
+                           author_id: @jin.id)
+      @post3 = Post.create(title: 'title 3',
+                           text: 'post content 3',
+                           comments_counter: 0,
+                           likes_counter: 0,
+                           author_id: @jin.id)
+      @post4 = Post.create(title: 'title 4',
+                           text: 'post content 4',
+                           comments_counter: 0,
+                           likes_counter: 0,
+                           author_id: @jin.id)
+      visit user_path(@jin.id)
+    end
 
-  it 'should show the profile picture for each user.' do
-    expect(page).to have_xpath("//img[contains(@src,'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg')]")
+    it 'should show the profile picture for each user.' do
+      expect(page).to have_xpath("//img[contains(@src,'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg')]")
+    end
+    it 'should show the username of all other users.' do
+      expect(page).to have_content(@jin.name)
+      expect(page).to_not have_content(@profile2.name)
+    end
+    it 'should show number of posts each user has written.' do
+      expect(page).to have_content 'Number of Posts: 4'
+    end
+    it 'I can see the user bio.' do
+      expect(page).to have_content 'Teacher from Mexico.'
+    end
+    it 'I can see the user first 3 posts.' do
+      expect(page).to_not have_content 'post content 4'
+    end
+    it 'I can see a button that lets me view all of a users posts.' do
+      expect(page).to have_content 'See more'
+    end
+    it 'When I click a users post, it redirects me to that posts show page.' do
+      visit "/users/#{@jin.id}/posts/#{@post1.id}"
+      expect(page).to have_content 'post content 1'
+    end
+    it 'When I click to see all posts, it redirects me to the users posts index page.' do
+      click_link 'See more'
+      expect(page).to have_content 'title 1'
+    end
+    it 'when click on user single post redirect me to post body' do
+      visit "/users/#{@jin.id}/posts/#{@post1.id}"
+      expect(page).to have_content 'Posted by: Jin'
+    end
   end
-  it 'should show the username of all other users.' do
-    expect(page).to have_content(@jin.name)
-    expect(page).to_not have_content(@profile2.name)
-  end
-  it 'should show number of posts each user has written.' do
-    expect(page).to have_content 'Number of Posts: 4'
-  end
-  it 'I can see the user bio.' do
-    expect(page).to have_content 'Teacher from Mexico.'
-  end
-  it 'I can see the user first 3 posts.' do
-    expect(page).to_not have_content 'post content 4'
-  end
-  it 'I can see a button that lets me view all of a users posts.' do
-    expect(page).to have_content 'See more'
-  end
-  it 'When I click a users post, it redirects me to that posts show page.' do
-    visit "/users/#{@jin.id}/posts/#{@post1.id}"
-    expect(page).to have_content 'post content 1'
-  end
-  it 'When I click to see all posts, it redirects me to the users posts index page.' do
-    click_link 'See more'
-    expect(page).to have_content 'title 1'
-  end
-  it "when click on user single post redirect me to post body" do
-    visit "/users/#{@jin.id}/posts/#{@post1.id}"
-    expect(page).to have_content 'Posted by: Jin'
-  end
-end
 end
