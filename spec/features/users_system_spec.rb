36332cbe7221e78350eda10d4c1a45b43d23d015
AbbasSarwar <abbassarwar100@gmail.com>
2023-07-25 18:21:48 +0500

    New tests for post & user single post

--- spec/features/users_system_spec.rb
+++ spec/features/users_system_spec.rb
@@ -10,35 +10,35 @@ RSpec.describe 'User', type: :system do
                             photo: 'https://as2.ftcdn.net/v2/jpg/02/14/74/61/1000_F_214746128_31JkeaP6rU0NzzzdFC4khGkmqc8noe6h.jpg',
                             bio: 'Teacher from Poland.',
                             post_counter: 0)
-                            @jin = User.create(name: 'Jin',
-                            photo: 'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg',
-                            bio: 'Teacher from Mexico.',
-                            post_counter: 0)
-                            
-         @post1 = Post.create(title: 'title 1',
-                              text: 'post content 1',
-                              comments_counter: 0,
-                              likes_counter: 0,
-                              author_id: @jin.id)
-         @post2 = Post.create(title: 'title 2',
-                              text: 'post content 2',
-                              comments_counter: 0,
-                              likes_counter: 0,
-                              author_id: @jin.id)
-         @post3 = Post.create(title: 'title 3',
-                              text: 'post content 3',
-                              comments_counter: 0,
-                              likes_counter: 0,
-                              author_id: @jin.id)
-         @post4 = Post.create(title: 'title 4',
-                              text: 'post content 4',
-                              comments_counter: 0,
-                              likes_counter: 0,
-                              author_id: @jin.id)
-        @comment1 = Comment.create(author_id: @profile1.id, post_id: @post1.id, text: "Hello")
-        @comment2 = Comment.create(author_id: @profile1.id, post_id: @post1.id, text: "wassup")
-        @like1 = Like.create(author_id: @profile1.id, post_id: @post1.id)
-
+    @jin = User.create(name: 'Jin',
+                       photo: 'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg',
+                       bio: 'Teacher from Mexico.',
+                       post_counter: 0)
+  end
+  before(:each) do
+    @post1 = Post.create(title: 'title 1',
+                         text: 'post content 1',
+                         comments_counter: 0,
+                         likes_counter: 0,
+                         author_id: @jin.id)
+    @post2 = Post.create(title: 'title 2',
+                         text: 'post content 2',
+                         comments_counter: 0,
+                         likes_counter: 0,
+                         author_id: @jin.id)
+    @post3 = Post.create(title: 'title 3',
+                         text: 'post content 3',
+                         comments_counter: 0,
+                         likes_counter: 0,
+                         author_id: @jin.id)
+    @post4 = Post.create(title: 'title 4',
+                         text: 'post content 4',
+                         comments_counter: 0,
+                         likes_counter: 0,
+                         author_id: @jin.id)
+    @comment1 = Comment.create(author_id: @profile1.id, post_id: @post1.id, text: 'Hello')
+    @comment2 = Comment.create(author_id: @profile1.id, post_id: @post1.id, text: 'wassup')
+    @like1 = Like.create(author_id: @profile1.id, post_id: @post1.id)
   end
   describe 'user index' do
     before(:each) do
@@ -97,52 +97,52 @@ RSpec.describe 'User', type: :system do
     end
   end
 
-  describe "post#index" do
-     before(:each) do
+  describe 'post#index' do
+    before(:each) do
       visit "/users/#{@jin.id}/posts"
-     end
+    end
     it 'User name show' do
-      expect(page).to have_content "#{@jin.name}"
+      expect(page).to have_content @jin.name.to_s
     end
     it 'User Profile picture' do
       expect(page).to have_xpath("//img[contains(@src,'https://as2.ftcdn.net/v2/jpg/03/26/98/51/1000_F_326985142_1aaKcEjMQW6ULp6oI9MYuv8lN9f8sFmj.jpg')]")
     end
     it 'Number of Posts user has written' do
-      expect(page).to have_content "Number of Posts: 4"
+      expect(page).to have_content 'Number of Posts: 4'
     end
     it 'Post title' do
-      expect(page).to have_content "title 1"
+      expect(page).to have_content 'title 1'
     end
     it 'Post body' do
-      expect(page).to have_content "post content 1"
+      expect(page).to have_content 'post content 1'
     end
     it 'comments show' do
-      expect(page).to have_content "Hello"
+      expect(page).to have_content 'Hello'
     end
-    it "comments count" do
-      expect(page).to have_content "comments: 2"
+    it 'comments count' do
+      expect(page).to have_content 'comments: 2'
     end
     it 'likes count' do
-      expect(page).to have_content "likes: 1"
+      expect(page).to have_content 'likes: 1'
     end
     it 'click on link redirect to page' do
-      click_link "post content 1"
+      click_link 'post content 1'
       expect(page).to have_current_path("/users/#{@jin.id}/posts/#{@post1.id}")
     end
   end
 
-  describe "Post#show" do
+  describe 'Post#show' do
     before(:each) do
       visit "/users/#{@jin.id}/posts/#{@post1.id}"
     end
     it 'post title' do
-      expect(page).to have_content "title 1"
+      expect(page).to have_content 'title 1'
     end
     it 'Author name' do
-      expect(page).to have_content "Posted by: Jin"
+      expect(page).to have_content 'Posted by: Jin'
     end
     it 'comments count show' do
-      expect(page).to have_content "comments: 2"
+      expect(page).to have_content 'comments: 2'
     end
     it 'likes count show' do
       expect(page).to have_content 'likes: 1'
@@ -151,8 +151,8 @@ RSpec.describe 'User', type: :system do
       expect(page).to have_content(@profile1.name)
     end
     it 'show all comments with name:' do
-      expect(page).to have_content "Tom: Hello"
-      expect(page).to have_content "Tom: wassup"
+      expect(page).to have_content 'Tom: Hello'
+      expect(page).to have_content 'Tom: wassup'
     end
   end
 end
