3feb5476a1cef35b2c85e7308e83522487ee3783
AbbasSarwar <abbassarwar100@gmail.com>
2023-07-26 18:30:14 +0500

    Devise added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  before_validation :set_default
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
@@ -7,8 +12,6 @@ class User < ApplicationRecord
   validates_associated :posts
 
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :bio, presence: true
-  validates :photo, presence: true
   validates :name, presence: true
 
   def recent_posts
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  devise_for :users
   root to: 'users#index'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id/posts', to: "posts#index", as: 'post'
-  get 'users/:id/posts/:id', to: "posts#show"
-  get '/posts/new', to: 'posts#new'
-  get '/users/:id/posts/:id/comments/new', to: 'comments#new'
 
   resources :users do
     resources :posts do
